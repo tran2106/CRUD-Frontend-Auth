104764cab72c181b6436445402f3ffe2cbd53099
Elian Echavarria <elianstanly431@gmail.com>
2025-07-07 14:40:44 -0400

    Added routes for LogIn and SignUp

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./NavBarStyles.css";
 
 const NavBar = () => {
-  return <nav className="navbar">{/* Some navbar stuff goes here */}</nav>;
+  return <nav className="navbar">
+    <a href="/login">LogIn</a>
+    <a href="/signup">SignUp</a>
+  </nav>;
 };
 
 export default NavBar;
